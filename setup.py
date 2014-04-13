d4a0a3543e6b0abdb58a6ed6a39e32d1a6ee3f2c
Guilherme Pontes <guilhermepontes@msn.com>
2014-04-13 15:28:11 -0300

    adding setup tools

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+def read(file):
+  return open(file, 'r+').read()
+    
+
+setup(
+  name = 'sivir',
+  version = read("version"),
+  url = 'https://github.com/guilhermepontes/sivir/',
+  license = 'GNU GPL',
+  author = 'Guilherme Pontes',
+  author_email='guilhermepontes@msn.com',
+  tests_require = ['unittest'],
+  install_requires = ['requests>=2.3.0'],
+  description = 'SIVIR: League of Legends API Library',
+  long_description = read("README.md"),
+  packages=['sivir'],
+  platforms='any',
+  classifiers = [
+    'Programming Language :: Python',
+    'Natural Language :: English',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU GPL',
+    'Operating System :: OS Independent'
+  ],
+  extras_require={
+    'testing': ['unittest']
+  }
+)
