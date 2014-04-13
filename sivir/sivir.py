9a7c83f3d72657fcf97d3e162d74be8049352767
Guilherme Pontes <guilhermepontes@msn.com>
2014-04-13 18:09:20 -0300

    adding sivir principal class

--- sivir/sivir.py
+++ sivir/sivir.py
@@ -1,1 +1,20 @@
-__version__ = 0.0.1
+import requests
+import sivir_request
+
+
+
+class Sivir(SivirRequest):
+	def __init__(self, key="123"):
+		if key == "":
+			raise Exception("You have to enter the API Key or Hecarim is going to be with you at night.")
+		else:
+			self.key = key
+
+	def from_region(self, region):
+		print region
+
+	def by_user(self, user):
+		print Sirvir
+
+
+s = SivirRequest("key", "params")
