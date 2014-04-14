2624f133a9a1ea9feae0280ba8367fbb4680a16c
Guilherme Pontes <guilhermepontes@msn.com>
2014-04-14 08:49:48 -0300

    class that will encapsulate all methods about a single summoner

---
+++ sivir/sivir_summoner.py
@@ -0,0 +1,11 @@
+from sivir_request import SivirRequest
+
+class SivirSummoner(SivirRequest):
+	def __init__(self): pass
+	
+	@classmethod
+	def get_data(self):
+		return SivirRequest(self.region, "/summoner/by-name/" + self.user).request()
+	
+	
+
