e5498de7eac1ff9194823e0e39f35cbf582d653d
Guilherme Pontes <guisantos@sp.r7.com>
2014-08-27 11:52:46 -0300

    adding a method to load config items

--- sivir/sivir_request.py
+++ sivir/sivir_request.py
@@ -1,30 +1,41 @@
-import requests
-import json
-import os
+import requests, json, os
+import sivir_messages
 
 class SivirRequest:
   def __init__(self, region = "", params = ""):
     if not all((region, params)):
-      raise Exception("You have to enter the <URL> AND <PARAMS> AND <REGION> or Hecarim is going to be with you at night.")
+      SivirMessages.error(["region", "params"])
     else:
       self.region = region
       self.params = params
       self.config()
 
+  @staticmethod
+  def load_config_file():
+    path        = os.path.dirname(os.path.abspath(__file__))
+    config_file = open(path + "/config/sivir_config.json").read()
+    return json.loads(config_file)
+
   @classmethod
   def config(self):
-    path        = os.path.abspath(".") + "/sivir/"
-    config_file = open(path + "config/sivir_config.json").read()
-    config      = json.loads(config_file)
+    config = self.load_config_file()
 
     self.__api_key        = config["api_key"]
     self.__api_version    = config["api_version"]
     self.__production_url = config["production_url"]
 
-  def request(self):
-    url = self.__production_url + self.region + "/" + self.__api_version + self.params
+  @classmethod
+  def make_url(self, prod, region, api_version, params):
+    url = prod + region + "/" + api_version + params
     url = url if url[-1:] == "/" else url + "/"
-    api = {"api_key": self.__api_key}
+    return url
 
+  @staticmethod
+  def make_api_param(key):
+    return {"api_key": key}
+
+  def request(self):
+    url = self.make_url(self.__production_url, self.region, self.__api_version, self.params)
+    api = self.make_api_param(self.__api_key)
     r = requests.get(url, params = api)
     return r.text
