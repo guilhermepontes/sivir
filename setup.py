ae24ec76118864cc95d7c4a90d60000a410fbcd0
Guilherme Pontes <guisantos@sp.r7.com>
2014-08-27 11:51:27 -0300

    old version of requests and intentend audience setted to web developers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 def read(file):
   return open(file, 'r+').read()
-    
+
 
 setup(
   name = 'sivir',
@@ -12,16 +12,16 @@ setup(
   author = 'Guilherme Pontes',
   author_email='guilhermepontes@msn.com',
   tests_require = ['unittest'],
-  install_requires = ['requests>=2.3.0'],
+  install_requires = ['requests>=2.2.1'],
   description = 'SIVIR: League of Legends API Library',
   long_description = read("README.md"),
-  packages=['sivir'],
+  packages=['sivir, tests'],
   platforms='any',
   classifiers = [
     'Programming Language :: Python',
     'Natural Language :: English',
     'Environment :: Web Environment',
-    'Intended Audience :: Developers',
+    'Intended Audience :: Web Developers',
     'License :: OSI Approved :: GNU GPL',
     'Operating System :: OS Independent'
   ],
